060393aa006b1ab81bff52a9d84ab8aa61adc2a8
Marina Galamdinova <mgalamdinova@gmail.com>
2023-07-27 22:57:33 +0300

    test frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
 
     return res.end();
   }
