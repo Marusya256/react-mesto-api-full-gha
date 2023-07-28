c8a708430884303b2a0d05a5188f7e17728ebf9d
Marina Galamdinova <mgalamdinova@gmail.com>
2023-07-28 11:24:10 +0300

    backend test

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
 
-    return res.end();
+    // return res.end();
   }
 
   next();
